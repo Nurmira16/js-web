dfbb274eadcdf85307a3bb4511124b8a4350e0a1
Nurmira16 <nurmirajorobek@gmail.com>
2023-11-26 10:59:19 +0100

    Fix bug of tab slider

--- js/lesson.js
+++ js/lesson.js
@@ -18,6 +18,7 @@ phone_button.addEventListener("click", () => {
 const tabsBlock = document.querySelectorAll(".tab_content_block");
 const tabItem = document.querySelectorAll(".tab_content_item");
 const itemBlock = document.querySelector(".tab_content_items");
+let currentcase = 0;
 const hide = () => {
   tabsBlock.forEach((block) => {
     block.style.display = "none";
@@ -26,18 +27,29 @@ const hide = () => {
     item.classList.remove("tab_content_item_active");
   });
 };
-const show = () => {
-  let index = 0;
-  setInterval(() => {
-    hide();
-    tabsBlock[index].style.display = "block";
-    tabItem.forEach((item) => {
-      item.classList.remove("tab_content_item_active");
-    });
-    tabItem[index].classList.add("tab_content_item_active");
-    index = (index + 1) % tabsBlock.length;
-  }, 2000);
+
+const show = (index = 0) => {
+  tabsBlock[index].style.display = "block";
+  tabItem[index].classList.add("tab_content_item_active");
+};
+const switchcase = () => {
+  hide();
+  currentcase = (currentcase + 1) % tabsBlock.length;
+  show(currentcase);
 };
 
 hide();
 show();
+setInterval(switchcase, 4000);
+
+itemBlock.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabItem.forEach((item, index) => {
+      if (event.target == item) {
+        hide();
+        currentcase = index;
+        show(currentcase);
+      }
+    });
+  }
+};
